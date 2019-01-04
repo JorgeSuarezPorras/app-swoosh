eab8df98b9295bab0c983df040efe75bc3f32eda
Jorge Suárez Porras <Jorge@Air-de-Jorge.home>
2019-01-04 14:19:16 +0100

    Optimize screens

--- App-Swoosh/App-Swoosh/ViewController.swift
+++ App-Swoosh/App-Swoosh/ViewController.swift
@@ -9,12 +9,23 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet var swoosh: UIImageView!
+    @IBOutlet var bgImg: UIImageView!
+    @IBOutlet var Button: BorderButton!
+    
+    @IBOutlet var Eslogan: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        swoosh.frame = CGRect(x: view.frame.size.width * 0.7 - swoosh.frame.size.width * 0.5, y:view.frame.size.height * 0.6 -  swoosh.frame.size.height, width: swoosh.frame.size.width , height: swoosh.frame.size.height )
+        bgImg.frame = view.frame
+        Button.frame = CGRect(x: view.frame.size.width * 0.25 - Button.frame.size.width * 0.5, y:view.frame.size.height * 0.75 -  Button.frame.size.height, width: Button.frame.size.width , height: Button.frame.size.height )
+        Eslogan.frame = CGRect(x: swoosh.frame.midX - swoosh.frame.size.width/2, y: swoosh.frame.midY+Eslogan.frame.size.height, width: Eslogan.frame.size.width, height: Eslogan.frame.size.height)
     }
 
-
+    
+    
 }
 
